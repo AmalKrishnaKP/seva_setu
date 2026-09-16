a1e1f5f45dd0b2c92252f5b0bc57442fce313e25
Amal Krishna K P <amalkrishnakp04@gmail.com>
2026-09-16 14:33:58 +0530

    setup: fronted:vite,backend:docker,alembic,sql session

---
+++ backend/src/core/session.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+
+load_dotenv()
+
+db_url=os.getenv("DB_URL")
+
+engin=create_engine(db_url)
+
+session= sessionmaker(autoflush=False,autocommit=False,bind=engin)
+
+def get_db():
+    db=session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+base=declarative_base()
